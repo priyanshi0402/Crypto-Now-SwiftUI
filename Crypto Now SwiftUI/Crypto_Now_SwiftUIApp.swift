f9a742753c5f45180c41ebb49019a7036bdcbd9f
Priyanshi Bhikadiya <priyanshibhikadiya.dev@gmail.com>
2023-09-10 19:41:18 +0530

    Initial Commit

---
+++ Crypto Now SwiftUI/Crypto_Now_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_Now_SwiftUIApp.swift
+//  Crypto Now SwiftUI
+//
+//  Created by Priyanshi Bhikadiya on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_Now_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
