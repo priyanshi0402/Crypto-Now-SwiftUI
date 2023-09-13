65a67e0758f61ae9c00e87334441d4feaa2947cd
Priyanshi Bhikadiya <priyanshibhikadiya.dev@gmail.com>
2023-09-13 13:37:42 +0530

    First commit

---
+++ Crypto Now SwiftUI/IntroView.swift
@@ -0,0 +1,126 @@
+//
+//  IntroView.swift
+//  Crypto Now SwiftUI
+//
+//  Created by Priyanshi Bhikadiya on 10/09/23.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    private var introArray: [IntroOption] = IntroOption.allCases
+    @State private var currentPage = 0
+    private let dotAppearance = UIPageControl.appearance()
+    //    var scrollPoxy: ScrollViewProxy!
+    
+    var body: some View {
+        VStack {
+            TabView(selection: $currentPage) {
+                ForEach(introArray) { page in
+                    VStack {
+                        OnboardingView(imageName: page.image, title: page.title)
+                        Spacer()
+                        Button {
+                            if page == introArray.last {
+                                
+                            } else {
+                                currentPage += 1
+                            }
+                        } label: {
+                            Text("Next")
+                                .frame(width: 300, height: 50)
+                                .background(Color.blue)
+                                .foregroundColor(Color.white)
+                        }
+                        .padding(.bottom, 100)
+       
+                    }
+                    .tag(page.tag)
+                    
+                }
+            }
+            .background(Color("BgColor"))
+            .animation(.easeInOut, value: currentPage)
+            .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+            .tabViewStyle(.page)
+            .onAppear {
+                dotAppearance.currentPageIndicatorTintColor = .white
+                dotAppearance.pageIndicatorTintColor = .gray
+            }
+            
+        }
+        .ignoresSafeArea()
+        
+    }
+    
+}
+
+struct IntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroView()
+    }
+}
+
+struct OnboardingView: View {
+    
+    let imageName: String
+    let title: String
+    
+    var body: some View {
+        
+        VStack(spacing: 20) {
+            Text(title)
+                .font(.title)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.white)
+                .padding(.leading)
+            
+            Image(imageName)
+                .resizable()
+                .padding(30)
+                .scaledToFit()
+            
+            Spacer()
+            
+        }
+        .padding(.top)
+       
+    }
+    
+}
+
+enum IntroOption: String, CaseIterable, Identifiable {
+    case first = "Boost your Crypto related decision"
+    case second = "Discover and monitor cryptocurrency related news worldwide"
+    case third = "Get the Crypto data within minutes after it is published"
+    
+    var id: String { rawValue }
+    
+    var title: String {
+        return rawValue
+    }
+    
+    var image: String {
+        switch self {
+        case .first:
+            return "intro_1"
+        case .second:
+            return "intro_2"
+        case .third:
+            return "intro_3"
+        }
+    }
+    
+    var tag: Int {
+        switch self {
+        case .first:
+            return 0
+        case .second:
+            return 1
+        case .third:
+            return 2
+        }
+    }
+    
+}
